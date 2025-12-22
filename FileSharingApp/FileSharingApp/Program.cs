020982e2d70131458059b8bbbcfa97c4ec365db7
Bloom Perez <springdotgay@tuta.io>
2025-12-22 12:55:16 -0400

    feat: remove Client project and add self-hosting (via docker)

--- FileSharingApp/FileSharingApp/Program.cs
+++ FileSharingApp/FileSharingApp/Program.cs
@@ -1,14 +1,36 @@
-using FileSharingApp.Client.Pages;
 using FileSharingApp.Components;
+using FileSharingApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
+    .AddInteractiveServerComponents(options =>
+    {
+        options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(10);
+    })
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+    options.UseSqlite("Data Source=data.db"));
+
+builder.Services.AddScoped<UserService>();
+
+builder.Services.AddServerSideBlazor()
+    .AddHubOptions(options =>
+    {
+        options.MaximumReceiveMessageSize = 1024 * 1024 * 500; // 500MB
+    });
+
 var app = builder.Build();
+
+// Ensure a database is created
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,15 +43,19 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
+app.UseStaticFiles();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(FileSharingApp.Client._Imports).Assembly);
+    .AddAdditionalAssemblies(typeof(_Imports).Assembly);
+
+app.UseStaticFiles();
 
 app.Run();
---
+++ README.md
@@ -0,0 +1,3 @@
+# FileShare
+
+A file sharing app written in C#, it was made in the middle of the night for no particular reason.
